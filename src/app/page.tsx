446b9137636b2781a1e12d03e4dc839b522af1bc
Cliff Barendsen <cliff.barendsen@gmail.com>
2025-10-17 22:36:38 -0400

    [chore] add feature products back to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,12 +29,12 @@ const HomePage = async () => {
     <>
       <Slider />
       {/* // FEATURED PRODUCTS */}
-      {/* <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
         <Suspense fallback={"loading"}>
           <ProductList categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!} limit={4} />
         </Suspense>
-      </div> */}
+      </div>
       {/* // CATEGORIES */}
       <div className="mt-24">
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">Categories</h1>
