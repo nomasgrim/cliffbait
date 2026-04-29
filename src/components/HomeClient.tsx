eadcdd4784383889594fbc7de6f193f1008882d5
Cliff Barendsen <cliff.barendsen@gmail.com>
2026-04-29 08:49:53 -0400

    bug(seo): addressed issue with metadata rendering in body tag.

---
+++ src/components/HomeClient.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Slider from "@/components/Slider";
+
+export default function HomeClient() {
+  return (
+    <section>
+      {/* LCP HERO ELEMENT */}
+      <div className="w-full">
+        <Slider />
+      </div>
+    </section>
+  );
+}
